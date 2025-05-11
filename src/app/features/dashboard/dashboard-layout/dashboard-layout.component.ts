036f4b035599246ede26a5440b44bcfa6cc330c5
rawannmmohamed <rawanmelkhodary@gmail.com>
2025-05-11 03:21:16 +0300

    ` updated dashboard layout component to display different columns based on user role`

--- src/app/features/dashboard/dashboard-layout/dashboard-layout.component.ts
+++ src/app/features/dashboard/dashboard-layout/dashboard-layout.component.ts
@@ -10,17 +10,41 @@ import { selectUser } from '../../../ngrx/auth/auth.selectors';
   styleUrl: './dashboard-layout.component.scss',
 })
 export class DashboardLayoutComponent {
+  avatar = '';
+  role = '';
+  tasks:any[] = [];
+  statuses: any[] | undefined;
+
   constructor(private store: Store) {
     this.store.select(selectUser).subscribe((user: User | null) => {
       if (user) {
         this.avatar = user.image;
+        this.role = user.role;
+        if (this.role === 'admin') {
+          this.tasks = [
+            {
+              code: 'A-001',
+              user: 'example@gmail.com',
+              title: 'Admin Task 1',
+              progress: 'completed',
+            },
+            {
+              code: 'A-002',
+              user: 'example@gmail.com',
+              title: 'Admin Task 2',
+              progress: 'pending',
+            },
+          ];
+        } else  {
+          this.tasks = [
+            { code: 'U-001', name: 'User Task 1', status: 'done' },
+            { code: 'U-002', name: 'User Task 2', status: 'in-progress' },
+          ];
+        }
       }
     });
   }
 
-  avatar = '';
-  tasks = [{ code: '22222', name: 'task1', status: 'done' }];
-  statuses: any[] | undefined;
   getSeverity(
     arg0: any
   ):
@@ -33,12 +57,15 @@ export class DashboardLayoutComponent {
     | undefined {
     throw new Error('Method not implemented.');
   }
+
   onRowEditInit(_t22: any) {
     throw new Error('Method not implemented.');
   }
+
   onRowEditSave(_t22: any) {
     throw new Error('Method not implemented.');
   }
+
   onRowEditCancel(_t22: any, _t24: any) {
     throw new Error('Method not implemented.');
   }
